14ed35acfea8824ce7c04755ff4d4f905ea6bda5
nadirkakoev <nadir_kakoev@yahoo.com>
2020-08-17 01:36:23 +0000

    a

---
+++ common_tools/google_k8s_sonarqube_module.tf
@@ -0,0 +1,13 @@
+module "sonarqube_deploy" {
+  source                 = "fuchicorp/chart/helm"
+  deployment_name        = "sonarqube"
+  deployment_environment = "${kubernetes_namespace.service_tools.metadata.0.name}"
+  deployment_endpoint    = "sonarqube.${var.google_domain_name}"
+  deployment_path        = "sonarqube"
+
+  template_custom_vars = {
+
+    null_depends_on      = "${null_resource.cert_manager.id}"
+    sonarqube_ip_ranges  =  "${var.sonarqube["sonarqube_ip_ranges"]}"
+  }
+}
